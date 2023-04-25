57cb4b8e092c81faab450737462de1ff588af69d
DietrichHeinz <23530835+DietrichHeinz@users.noreply.github.com>
2023-04-24 22:07:20 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function() {
-  var heroSectionHeight = document.getElementById("hero-section").offsetHeight;
-  if (window.pageYOffset > heroSectionHeight - 100) {
-    document.getElementById("cat-section").classList.add("show");
-    document.getElementById("hero-section").classList.add("hide");
-  }
-});
